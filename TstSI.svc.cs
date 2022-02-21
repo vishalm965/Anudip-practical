b9476a579ff51e87d21875fe8e1fe50eb848e5f3
Vishal Mandal <94603068+vishalm965@users.noreply.github.com>
2022-02-21 10:37:59 +0530

    Add files via upload

---
+++ TstSI.svc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfTst
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "TstSI" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select TstSI.svc or TstSI.svc.cs at the Solution Explorer and start debugging.
+    public class TstSI : ITstSI
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=VISHAL;Initial Catalog=vishal;Integrated Security=True");
+        public int CalculateSI(int p, int r, int t)
+        {
+            int SI;
+            SI = (p * r * t) / 100;
+            return SI;
+        }
+        public DataTable GetRecordList()
+        {
+            SqlDataAdapter da = new SqlDataAdapter();
+            da = new SqlDataAdapter("select * from student9", con);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                dt = ds.Tables[0];
+            }
+            return dt;
+        }
+
+    }
+    }
+
